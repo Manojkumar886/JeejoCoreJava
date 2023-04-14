2852ae3e792ff97a9b3fd23e0aba8fd5730d28ae
Manojkumar886 <maddymano434@gmail.com>
2023-04-14 14:46:31 +0530

    Initial commit and array perforamnce is done

--- src/main/java/FundamentalJava/ArrayImplementation/SimpleTask.java
+++ src/main/java/FundamentalJava/ArrayImplementation/SimpleTask.java
@@ -1,2 +1,53 @@
-package FundamentalJava.ArrayImplementation;public class SimpleTask {
+package FundamentalJava.ArrayImplementation;
+
+import java.util.Arrays;
+
+public class SimpleTask
+{
+
+   private int [][] TwodArray={{12,13,14},{56,57,58},{97,98,99}};
+    public void Implementaion()
+    {
+        for(int row=0;row<TwodArray.length;row++)
+        {
+            for(int col=0;col<TwodArray.length;col++)
+            {
+                System.out.print(TwodArray[row][col]+" ");
+            }
+            System.out.println();
+        }
+
+        int [] convertingsingle=new int[TwodArray.length*3];
+        int row=2,col=0,index=0;
+        for(;col<TwodArray.length;col++,index++)
+        {
+            convertingsingle[index]=TwodArray[row][col];
+        }
+        row--;col--;//col=2,row=1
+        for(;row>=0;row--,index++)
+        {
+            convertingsingle[index]=TwodArray[row][col];
+        }
+        col--;row++;
+        for(;col>=0;col--,index++)
+        {
+            convertingsingle[index]=TwodArray[row][col];
+        }
+        row++;col++;
+        for(;col<=1;col++,index++)
+        {
+            convertingsingle[index]=TwodArray[row][col];
+        }
+//        [57,58,14,13,12,56,99,98,97]
+
+        System.out.println(Arrays.toString(convertingsingle));
+
+    }
+    public static void main(String[] args)
+    {
+
+        SimpleTask task1=new SimpleTask();
+        task1.Implementaion();
+
+    }
 }
